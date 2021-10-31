8152a8a595b523c0df5ef27c8e70ae936f6e254a
Rainwang <993789027@qq.com>
2021-10-31 19:45:17 +0800

    mvc

--- src/app4.js
+++ src/app4.js
@@ -5,7 +5,6 @@ const $circle = $('#app4 .circle')
 
 $circle.on('mouseenter', ()=> {
     $circle.addClass('active')
-        .on('mouseleave',()=>{
-            $circle.removeClass('active')
-        })
+}).on('mouseleave',()=>{
+    $circle.removeClass('active')
 })
